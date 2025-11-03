dd22687dc24227e811addc4f828877ed172da847
Arthur_Linux <arthur@ioda.me>
2025-11-03 16:59:57 +0100

    final data frame implement

---
+++ code_exe/load_files.py
@@ -0,0 +1,50 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+import io, os, glob
+
+def download_files_func():
+    # Configuration
+    SERVICE_ACCOUNT_FILE = glob.glob("/opt/spark/google_auth/e-checks-project*.json")[0]
+    FOLDER_ID = "1uW77S7QK2xsk-M6urShuGy2HqTpnKykd"
+    LOCAL_DIR = "/opt/spark/source_data"
+
+    # Authenticate
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE,
+        scopes=["https://www.googleapis.com/auth/drive"]
+    )
+
+    drive_service = build("drive", "v3", credentials=creds)
+
+    # List all CSV files in the folder
+    query = f"'{FOLDER_ID}' in parents and (mimeType='text/csv' or mimeType='application/json') and trashed=false" 
+    results = drive_service.files().list(q=query, fields="files(id, name)").execute()
+    files = results.get("files", [])
+
+
+
+    if not files:
+        print("No files found")
+    else:
+        # Download files
+        for file in files:
+            file_id = file["id"]
+            file_name = file["name"]
+            local_path = os.path.join(LOCAL_DIR, file_name)
+
+            print(f"Downloading: {file_name} ...")
+
+            request = drive_service.files().get_media(fileId=file_id)
+            fh = io.FileIO(local_path, "wb")
+
+            downloader = MediaIoBaseDownload(fh, request)
+            done = False
+
+            while not done:
+                status, done = downloader.next_chunk()
+                if status:
+                    print(f"Progress: {int(status.progress() * 100)}%")
+            
+            fh.close()
+            print(f"Downloaded: {file_name}")
--- code_exe/main.py
+++ code_exe/main.py
@@ -1,14 +1,39 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
+from pyspark.sql.functions import col
 from biedronka_e_check_code.parse_json_b_check import run_biedronka_execution
+from lidl_e_check_code.parse_csv_lidl_check import run_lidl_execution
+from load_files import download_files_func
+
+download_files_func()
 
 spark = SparkSession.builder.appName("E-check transformator").getOrCreate()
 
 df_json = spark.read.option("multiline", True).json("/opt/spark/code_exe/category_type.json")
+
+# Reading biedroka e-check files
 df_bied = spark.read.option("multiline", True).json("/opt/spark/source_data/")
+
+# Reading lidl e-check files
+schema = StructType([
+        StructField("item_name", StringType(), True),
+        StructField("quantity", DoubleType(), True),
+        StructField("price", DoubleType(), True),
+        StructField("discount_value", DoubleType(), True),
+        StructField("discount", StringType(), True),
+        StructField("time", StringType(), True),
+        StructField("date", StringType(), True)
+    ])
+
+df_lidl = spark.read.csv("/opt/spark/source_data/", header=True, schema=schema)
 
 # Execute transformations
 df_bied = run_biedronka_execution(df_bied, df_json)
+df_lidl = run_lidl_execution(df_lidl, df_json)
 
-df_bied.show()
+df_final = df_bied.union(df_lidl)
+df_final = df_final.orderBy(col("date").asc())
+
+df_final.coalesce(1).write.mode("overwrite").option("header", True).csv("/opt/spark/sink_data/csv_file")
 
 spark.stop()
